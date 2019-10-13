b8ccadf2240927d9e9478ac9a39fb7bc94f990f7
tehZevo <tehzevo@users.noreply.github.com>
2019-10-12 23:32:16 -0400

    Add preprocess_inputs to KerasAppEngine

--- aegis_core/keras_app_engine.py
+++ aegis_core/keras_app_engine.py
@@ -6,23 +6,24 @@ from .engine import RequestEngine
 from tensorflow.keras.applications import VGG16, VGG19, ResNet50, InceptionV3, \
   InceptionResNetV2, Xception, MobileNet, MobileNetV2, DenseNet121, DenseNet169, \
   DenseNet201, NASNetMobile, NASNetLarge
+import tensorflow.keras.applications as apps
 
 #TODO: not sure if this is needed...
-#class, default input size, default output size, output depth
+#class, default input size, default output size, output depth, preprocessor
 apps = {
-  "vgg16": (VGG16, 224, 7, 512),
-  "vgg19": (VGG19, 224, 7, 512),
-  "resnet50": (ResNet50, 224, 7, 2048),
-  "inception_v3": (InceptionV3, 299, 8, 2048),
-  "inception_resnet_v2": (InceptionResNetV2, 299, 8, 1536),
-  "xception": (Xception, 299, 10, 2048),
-  "mobilenet": (MobileNet, 224, 7, 1024),
-  "mobilenet_v2": (MobileNetV2, 224, 7, 1280),
-  "densenet121": (DenseNet121, 224, 7, 1024),
-  "densenet169": (DenseNet169, 224, 7, 1664),
-  "densenet201": (DenseNet201, 224, 7, 1920),
-  "nasnet_mobile": (NASNetMobile, 224, 7, 1056),
-  "nasnet_large": (NASNetLarge, 331, 11, 4032),
+  "vgg16": (VGG16, 224, 7, 512, apps.vgg16.preprocess_input),
+  "vgg19": (VGG19, 224, 7, 512, apps.vgg19.preprocess_input),
+  "resnet50": (ResNet50, 224, 7, 2048, apps.resnet50.preprocess_input),
+  "inception_v3": (InceptionV3, 299, 8, 2048, apps.inception_v3.preprocess_input),
+  "inception_resnet_v2": (InceptionResNetV2, 299, 8, 1536, apps.inception_resnet_v2.preprocess_input),
+  "xception": (Xception, 299, 10, 2048, apps.xception.preprocess_input),
+  "mobilenet": (MobileNet, 224, 7, 1024, apps.mobilenet.preprocess_input),
+  "mobilenet_v2": (MobileNetV2, 224, 7, 1280, apps.mobilenet_v2.preprocess_input),
+  "densenet121": (DenseNet121, 224, 7, 1024, apps.densenet.preprocess_input),
+  "densenet169": (DenseNet169, 224, 7, 1664, apps.densenet.preprocess_input),
+  "densenet201": (DenseNet201, 224, 7, 1920, apps.densenet.preprocess_input),
+  "nasnet_mobile": (NASNetMobile, 224, 7, 1056, apps.nasnet.preprocess_input),
+  "nasnet_large": (NASNetLarge, 331, 11, 4032, apps.nasnet.preprocess_input),
   #TODO: included in tf 2.0
   #"resnet101": ResNet101,
   #"resnet152": ResNet152,
@@ -38,9 +39,11 @@ class KerasAppEngine(RequestEngine):
   def __init__(self, app_name="mobilenet_v2", pooling="max", input_urls=[]):
     super().__init__(input_urls=input_urls)
 
-    app, in_size, out_size, out_depth = apps[app_name]
+    app, in_size, out_size, out_depth, preprocessor = apps[app_name]
     self.model = app(include_top=False, weights="imagenet",
       input_shape=None, pooling=pooling)
+
+    self.preprocess_input = preprocessor
 
     self.input_shape = [in_size, in_size, 3]
     self.output_shape = [out_depth]
@@ -49,6 +52,8 @@ class KerasAppEngine(RequestEngine):
     input_states = self.get_inputs(0) #dont propagate reward
     #fix Nones
     input_states = [np.zeros(self.input_shape) if x is None else x for x in input_states]
+    #preprocess inputs
+    input_states = [self.preprocess_input(x) for x in input_states]
     #TODO: batch if all inputs are guaranteed to be the same dimensions?
     output_states = [self.model(input_state) for input_state in input_states]
     output_state = np.mean(output_states, axis=0)
