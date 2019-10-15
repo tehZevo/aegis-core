14b7371e333dddae0d2d81e882aeba97edcfa8f4
tehZevo <tehzevo@users.noreply.github.com>
2019-10-15 17:27:03 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='aegis_core',
-  version='0.4.0',
+  version='0.5.0',
   install_requires = [
     'ml_utils @ git+https://github.com/tehzevo/ml-utils@master#egg=ml_utils',
     'pget @ git+https://github.com/tehzevo/pget@master#egg=pget',
