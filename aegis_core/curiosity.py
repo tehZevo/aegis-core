3e17bf20a764cc923371af507d230f9ec53da542
tehZevo <tehzevo@users.noreply.github.com>
2019-10-14 22:23:10 -0400

    Fix self.buffer reference in LocalCuriosityEngine

--- aegis_core/curiosity.py
+++ aegis_core/curiosity.py
@@ -86,7 +86,7 @@ class LocalCuriosityEngine(RequestEngine):
 
     #train
     if train:
-      batch = random.sample(deq, self.batch_size)
+      batch = random.sample(self.buffer, self.batch_size)
       x = np.array(batch)
       train_loss = self.model.train_on_batch(x, x)
 
