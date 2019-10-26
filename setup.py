249c23d4a60b12d6b262ba85a2007d36a7b7e4d4
tehZevo <tehzevo@users.noreply.github.com>
2019-10-26 15:05:36 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='aegis_core',
-  version='0.6.0',
+  version='0.7.0',
   install_requires = [
     'ml_utils @ git+https://github.com/tehzevo/ml-utils@master#egg=ml_utils',
     'tensorflow',
