Fix issue in reward proxy deviation calculation

aegis_core/reward_proxy.py
@@ -84,8 +84,9 @@ class RewardProxy(RequestEngine):
       d_reward = r - mean
       normalized_reward = d_reward / (deviation + self.epsilon)
       #update mean/deviation
+      a += (b - a) * t
       self.means[name] += d_reward * decay_rate
-      self.deviations[name] += (deviation - abs(d_reward)) * decay_rate
+      self.deviations[name] += (abs(d_reward) - deviation) * decay_rate
       #clip and scale reward
       if clip is not None:
         normalized_reward = np.clip(normalized_reward, -clip, clip)